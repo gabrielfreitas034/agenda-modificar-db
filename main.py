d34d497aac85004449379aff9eaa056f7856ed96
Gabriel Freitas <101368233+034Gabriel@users.noreply.github.com>
2022-04-28 12:34:23 +0000

    com login e register

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
-
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask('app')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -65,6 +65,57 @@ def update(id):
   cont.phone = phone
   db.session.commit()  
   return redirect('/')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/register')
+def register():
+  return render_template('register.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+  name_input = request.form.get('name')
+  email_input = request.form.get('email')
+  password_input = request.form.get('password')
+  
+  # Verificar se já existe o email no bd
+  user = Users.query.filter_by(email=email_input).first()
+  if user:
+    return redirect('/register')
+
+  new_user = Users(
+    name=name_input,
+    email=email_input,
+    password=generate_password_hash(password_input)
+  )
+  db.session.add(new_user)
+  db.session.commit()
+  return redirect('/login')
+
+@app.route('/signin', methods=['POST'])
+def signin():
+  email_input = request.form.get('email')
+  password_input = request.form.get('password')
+
+  # Verificar se existe um usário com o email 
+  user = Users.query.filter_by(email=email_input).first()
+  if not user:
+    return redirect('/login')
+  
+  # Verificar se senha está correta
+  if not check_password_hash(user.password, password_input):
+    return redirect('/login')
+
+  # Guardar usuário na sessão
+  session['user_id'] = user.id
+  return redirect('/')
+
+@app.route('/logout')
+def logout():
+  session.pop('user_id', None)
+  return redirect('/login')
 
 # IMPORTANTE V
 if __name__ == '__main__':
